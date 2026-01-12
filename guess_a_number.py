1a0e53e8fd5eda32e0bd1b04d75dd6bf8bfdae9b
olgababitskaya <shevchonok@gmail.com>
2026-01-12 17:48:02 +0200

    Added continue question

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,16 +1,23 @@
 from random import randint
-computer_number = randint(1, 100)
 while True:
-    player_input = input("Guess a number between 1 and 100: ")
-    if not player_input.isdigit():
-        print("Please enter a number")
-        continue
-    else:
-        trying_number = int(player_input)
-        if computer_number == trying_number:
-            print("You guessed the number")
-            break
-        elif trying_number > computer_number:
-            print("Too high")
-        elif trying_number < computer_number:
-            print("Too low")
+
+    computer_number = randint(1, 100)
+    while True:
+        player_input = input("Guess a number between 1 and 100: ")
+        if not player_input.isdigit():
+            print("Please enter a number")
+            continue
+        else:
+            trying_number = int(player_input)
+            if computer_number == trying_number:
+                print("You guessed the number")
+                break
+            elif trying_number > computer_number:
+                print("Too high")
+            elif trying_number < computer_number:
+                print("Too low")
+
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again != "y":
+        print("Thanks for playing!")
+        break
